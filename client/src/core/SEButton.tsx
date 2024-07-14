96430b89289ad25bc9957184afa931da4d3a8510
Mohamed <93926306+mnogot098@users.noreply.github.com>
2024-07-14 22:07:54 +0000

    FE:Setup button component

--- client/src/core/SEButton.tsx
+++ client/src/core/SEButton.tsx
@@ -1,46 +1,42 @@
-import { Button } from 'antd';
-import { FC } from 'react';
-import React from 'react';
-
+import { Button, ButtonProps } from "antd";
+import { CSSProperties, FC } from "react";
+import React from "react";
 
 interface SEButtonProps {
   label: string;
   onClick: () => void;
   disabled?: boolean;
-  type?: "default" | "primary" | "ghost" | "dashed" | "link" | "text";
+  type?: ButtonProps['type'];
   id?: string;
   dataTestId?: string;
   ariaLabel?: string;
   confirm?: string; // New prop for confirmation dialog
+  style?:CSSProperties
+  
 }
 
-const SEButton: FC<SEButtonProps> = ({
-  label,
-  onClick,
-  disabled = false,
-  type = "default",
-  id,
-  dataTestId,
-  ariaLabel,
-  confirm,
-}) => {
-
+const SEButton: FC<SEButtonProps> = (params: SEButtonProps) => {
   const handleClick = () => {
-    if (confirm && window.confirm(confirm)) {
-      onClick(); // Call the onClick function if confirmed
+    if (params.confirm) {
+      if (window.confirm(params.confirm)) {
+        params.onClick();
+      }
+    } else {
+      params.onClick();
     }
   };
 
   return (
     <Button
       onClick={handleClick}
-      disabled={disabled}
-      id={id}
-     
-      data-testid={dataTestId}
-      aria-label={ariaLabel}
+      disabled={params.disabled}
+      type={params.type}
+      aria-label={params.ariaLabel}
+      id={params.id}
+      data-testid={params.dataTestId}
+      style={{...params.style}}
     >
-      {label}
+      {params.label}
     </Button>
   );
 };

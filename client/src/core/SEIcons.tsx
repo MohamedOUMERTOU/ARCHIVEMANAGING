841a5624c0d0446febbeaa8ac27bf3070e76c9b3
Mohamed <93926306+mnogot098@users.noreply.github.com>
2024-07-15 15:12:44 +0000

    add icons component

---
+++ client/src/core/SEIcons.tsx
@@ -0,0 +1,8 @@
+// src/core/SEIcons.tsx
+
+import { PlusCircleFilled } from '@ant-design/icons';
+import React from 'react';
+
+export const Icons = {
+   PlusCircleIcon: (props:any) => <PlusCircleFilled {...props} />
+};

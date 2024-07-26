0ef596ab85f94666b44927bb0d1edbcbd4d5ef2b
Mohamed <mnogot@orsys.co.ma>
2024-07-26 23:20:43 +0000

    text input

--- client/src/core/SETextInputs.tsx
+++ client/src/core/SETextInputs.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Input } from "antd";
+import { Input, Form } from "antd";
 import type { InputProps } from "antd/lib/input";
 import SECol from "./SECol";
 import SETypography from "./SETypography"; // Import SETypography for displaying the label
 
 interface SETextInputProps extends Omit<InputProps, "onChange"> {
-  value: string;
   onChange: (value: string) => void;
   placeholder?: string;
   disabled?: boolean;
@@ -15,10 +14,10 @@ interface SETextInputProps extends Omit<InputProps, "onChange"> {
   tabIndex?: number;
   col?: number;
   text?: string; // Add text prop for the label
+  name: string; // Add name prop for form registration
 }
 
 const SETextInput: React.FC<SETextInputProps> = ({
-  value,
   onChange,
   placeholder = "",
   disabled = false,
@@ -28,31 +27,28 @@ const SETextInput: React.FC<SETextInputProps> = ({
   tabIndex,
   col = 24,
   text,
+  name,
   ...restProps
 }) => {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     onChange(event.target.value);
   };
 
-  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
-    // Optional: Handle onBlur events if needed
-  };
-
   return (
     <SECol span={col}>
-      {text && <SETypography text={text} level={5}/>} {/* Render label if text is provided */}
-      <Input
-        className={`se-text-input ${className}`}
-        value={value}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        placeholder={placeholder}
-        disabled={disabled}
-        id={id}
-        autoFocus={autoFocus}
-        tabIndex={tabIndex}
-        {...restProps} // Spread remaining props to Ant Design Input component
-      />
+      {text && <SETypography text={text} level={5} />} {/* Render label if text is provided */}
+      <Form.Item name={name} rules={[{ required: true, message: `${text} is required` }]}>
+        <Input
+          className={`se-text-input ${className}`}
+          onChange={handleChange}
+          placeholder={placeholder}
+          disabled={disabled}
+          id={id}
+          autoFocus={autoFocus}
+          tabIndex={tabIndex}
+          {...restProps} // Spread remaining props to Ant Design Input component
+        />
+      </Form.Item>
     </SECol>
   );
 };
